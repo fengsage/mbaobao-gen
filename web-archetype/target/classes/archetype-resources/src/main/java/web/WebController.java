bb0d72e53dbba816bd03314829bb8ac894c9c03f
fredzhu <me@fengsage.com>
2012-07-17 23:39:39 +0800

    init project

---
+++ web-archetype/target/classes/archetype-resources/src/main/java/web/WebController.java
@@ -0,0 +1,37 @@
+/**
+ * www.mbaobao.com Inc.
+ * Copyright (c) 2011 All Rights Reserved.
+ */
+
+package ${groupId}.web;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ *	Create by mabaobao-web-archetype
+ *
+ */
+@Controller
+public class WebController {
+	
+	
+	@RequestMapping(value = "/index.html", method = RequestMethod.GET)
+	public String index(HttpServletRequest request, HttpServletResponse response,
+						Map<String, Object> modelMap) throws UnsupportedEncodingException {
+		
+		return "index";
+	}
+}
