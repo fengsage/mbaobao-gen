cc0f7cf7dceca64b2c519fbf6781f959d379f216
fredzhu <me@fengsage.com>
2012-07-18 21:57:01 +0800

    完成基本功能。项目可以运行。

---
+++ web-archetype/src/main/resources/archetype-resources/src/main/java/dao/DemoDao.java
@@ -0,0 +1,12 @@
+/**
+ * www.mbaobao.com Inc.
+ * Copyright (c) 2011 All Rights Reserved.
+ */
+package ${groupId}.dao;
+/**
+ *	Create by mabaobao-web-archetype
+ *
+ */
+public interface DemoDao {
+	
+}
--- web-archetype/target/classes/archetype-resources/src/main/java/web/WebController.java
+++ web-archetype/target/classes/archetype-resources/src/main/java/web/WebController.java
@@ -5,16 +5,10 @@
 
 package ${groupId}.web;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
