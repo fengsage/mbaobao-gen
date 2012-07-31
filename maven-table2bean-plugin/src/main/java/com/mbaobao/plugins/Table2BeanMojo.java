76d3f940a6714459148b7e2c17bdd40a0cde19ac
fredzhu <me@fengsage.com>
2012-07-31 22:15:22 +0800

    还不能用plugin

--- maven-table2bean-plugin/src/main/java/com/mbaobao/plugins/Table2BeanMojo.java
+++ maven-table2bean-plugin/src/main/java/com/mbaobao/plugins/Table2BeanMojo.java
@@ -4,51 +4,31 @@
  */
 package com.mbaobao.plugins;
 
-import java.util.List;
+import java.sql.SQLException;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
 import com.mbaobao.gen.GenFactory;
-import com.mbaobao.gen.entity.TableMapping;
-import com.mbaobao.plugins.entity.DatabaseConfig;
-import com.mbaobao.plugins.entity.MavenConfiguration;
-import com.mbaobao.plugins.entity.ResourceConfig;
 
 /**
  * @goal gen
  */
 public class Table2BeanMojo extends AbstractMojo {
 	
-	/**
-	 * @parameter
-	 */
-	private ResourceConfig		resource;
-	
-	/**
-	 * @parameter
-	 */
-	private DatabaseConfig		database;
-	
-	/**
-	 *  @parameter
-	 */
-	private List<TableMapping>	mappings;
-	
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		getLog().info("===============Table2Bean===============");
-		
-		getLog().info("DataBase Configuration:" + database);
-		getLog().info("Resource Configuration:" + resource);
-		getLog().info("TableMapping Configuration:" + mappings);
-		
 		long startTime = System.currentTimeMillis();
 		
-		new GenFactory(new MavenConfiguration(database, resource, mappings));
+		try {
+			new GenFactory(new PropertiesConfiguration()).export();
+		} catch (SQLException e) {
+			getLog().error("", e);
+		}
 		
-		getLog().info("Gen Database wast:" + (System.currentTimeMillis() - startTime));
+		getLog().info("Gen wast:" + (System.currentTimeMillis() - startTime));
 	}
 	
 }
